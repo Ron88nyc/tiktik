69c0f20d8dbdf42e36881755a09732d757f6bdd7
Ronald Li <Ronaldnyc88@gmail.com>
2022-11-16 14:12:05 -0500

    Updated first half refactor

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains:['yt3.ggpht.com',
+    "lh3.googleusercontent.com"]
+  }
 }
 
 module.exports = nextConfig
